93d3d388f20a06b1438fd3db3b01115ecf75b7cc
Ruthik Garapati <garapatiruthik@gmail.com>
2026-02-28 11:39:29 +0100

    Fix: Remove relative imports from __init__.py

--- src/inerb/__init__.py
+++ src/inerb/__init__.py
@@ -1,16 +1,25 @@
 """
 INERB - Drunk/Sober Detection System
-
-A machine learning application that detects whether a person is drunk or sober
-based on facial features extracted using Mediapipe.
 """
 
 __version__ = "1.0.0"
 __author__ = "INERB Team"
 
-from .features import extract_features, get_landmarks
-from .model import train_model, predict, load_model, save_model
-from .dataset import load_dataset, export_features_to_csv
+# Import without relative paths to avoid issues
+import features
+import model
+import dataset
+import utils
+
+# Export main functions
+extract_features = features.extract_features
+get_landmarks = features.get_landmarks
+train_model = model.train_model
+predict = model.predict
+load_model = model.load_model
+save_model = model.save_model
+load_dataset = dataset.load_dataset
+export_features_to_csv = dataset.export_features_to_csv
 
 __all__ = [
     "extract_features",
