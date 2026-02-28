670a924a2a1684b3986531231b32510e4eaa2f71
Ruthik Garapati <garapatiruthik@gmail.com>
2026-02-28 12:20:08 +0100

    Fix: Simplify __init__.py imports

--- src/inerb/__init__.py
+++ src/inerb/__init__.py
@@ -5,21 +5,10 @@ INERB - Drunk/Sober Detection System
 __version__ = "1.0.0"
 __author__ = "INERB Team"
 
-# Use dot notation for relative imports
-from . import features
-from . import model
-from . import dataset
-from . import utils
-
-# Export main functions
-extract_features = features.extract_features
-get_landmarks = features.get_landmarks
-train_model = model.train_model
-predict = model.predict
-load_model = model.load_model
-save_model = model.save_model
-load_dataset = dataset.load_dataset
-export_features_to_csv = dataset.export_features_to_csv
+# Export main functions explicitly
+from .features import extract_features, get_landmarks
+from .model import train_model, predict, load_model
+from .dataset import load_dataset, export_features_to_csv
 
 __all__ = [
     "extract_features",
@@ -27,7 +16,6 @@ __all__ = [
     "train_model",
     "predict",
     "load_model",
-    "save_model",
     "load_dataset",
     "export_features_to_csv",
 ]
